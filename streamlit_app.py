45499c60e88ac18255056e5baeef9790cfed4e66
John Michael <67716815+johnmikel306@users.noreply.github.com>
2024-10-17 09:21:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
-import os
 import streamlit as st
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import Flow
-from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import UnstructuredFileIOLoader
@@ -14,29 +11,32 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_community.vectorstores import FAISS
 
-# Retrieve environment variables
+# Scopes required for Google Drive access
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
+# Function to get or refresh credentials
+def get_credentials():
+    creds = None
+    if 'token' in st.secrets:
+        creds = Credentials.from_authorized_user_info(st.secrets['token'], SCOPES)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+            # Update the secrets with the refreshed token
+            st.secrets['token'] = creds.to_json()
+        else:
+            st.error("Credentials are invalid or missing. Please update the app secrets.")
+            st.stop()
+    
+    return creds
 
 # Function to load Google Drive documents with caching
 @st.cache_data(show_spinner=True, max_entries=10)
 def load_google_drive_documents():
-    creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = Flow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
+    creds = get_credentials()
     loader = GoogleDriveLoader(
-        folder_id=os.getenv("FOLDER_ID"),
+        folder_id=st.secrets['FOLDER_ID'],
         credentials=creds,
         file_types=["document", "sheet"],
         file_loader_cls=UnstructuredFileIOLoader,
@@ -44,7 +44,6 @@ def load_google_drive_documents():
         recursive=False,
     )
     return loader.load()
-
 
 # Function to combine and split documents
 def split_documents(all_docs):
@@ -57,23 +56,18 @@ def split_documents(all_docs):
     splits = text_splitter.split_documents(all_docs)
     return splits
 
-
 # Function to create vector store with caching
 @st.cache_resource(show_spinner=True)
 def create_vector_store(_splits):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=st.secrets['GOOGLE_API_KEY'])
     return FAISS.from_documents(_splits, embeddings)
-
 
 # Function to initialize and return the RAG chain
 def create_rag_chain(db):
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",
-                                 google_api_key=GOOGLE_API_KEY)
-    retriever = db.as_retriever(search_type='similarity',
-                                search_kwargs={"k": 4})
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=st.secrets['GOOGLE_API_KEY'])
+    retriever = db.as_retriever(search_type='similarity', search_kwargs={"k": 4})
     prompt = ChatPromptTemplate.from_messages([
-        ("human",
-         """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know as it is not included in the Google Drive you're provided.
+        ("human", """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know as it is not included in the Google Drive you're provided.
     For phone numbers, always make sure to add "+234" infront of the number if there is no "0" as the first number, else replace the starting "0" with "+234".
     For amounts in Naira, add the Naira sign infront of the figure. Do the same for amounts in dollars. 
     When asked questions whose answers have urls included, make sure to provide the url embedded in the response as well.
@@ -87,15 +81,11 @@ def create_rag_chain(db):
     def format_docs(docs):
         return "\n\n".join(doc.page_content for doc in docs)
 
-    rag_chain = ({
-        "context": retriever | format_docs,
-        "question": RunnablePassthrough()
-    }
+    rag_chain = ({"context": retriever | format_docs, "question": RunnablePassthrough()}
                  | prompt
                  | llm
                  | StrOutputParser())
     return rag_chain
-
 
 # Function to handle the chat interface
 def render_chat_interface(rag_chain):
@@ -120,47 +110,21 @@ def render_chat_interface(rag_chain):
 
         st.session_state['chat_history'] = chat_history
 
-
 # Main function for the Streamlit app
 def main():
     st.title("MIVA Success Advisor's Assistant")
 
-    if not os.path.exists('token.json'):
-        st.warning(
-            "You need to authorize the application. Click the button below to start the authorization process."
-        )
-        if st.button("Authorize"):
-            flow = Flow.from_client_secrets_file(
-                'credentials.json',
-                SCOPES,
-                redirect_uri='urn:ietf:wg:oauth:2.0:oob')
-            auth_url, _ = flow.authorization_url(prompt='consent')
-            st.write(
-                f"Please visit this URL to authorize the application: {auth_url}"
-            )
-            auth_code = st.text_input("Enter the authorization code:")
-            if auth_code:
-                flow.fetch_token(code=auth_code)
-                creds = flow.credentials
-                with open('token.json', 'w') as token:
-                    token.write(creds.to_json())
-                st.success(
-                    "Authorization successful! You can now use the application."
-                )
-                st.experimental_rerun()
-    else:
+    try:
         with st.spinner("Loading documents from Google Drive..."):
-            try:
-                drive_docs = load_google_drive_documents()
-            except Exception as e:
-                st.error(f"Error loading documents: {str(e)}")
-                return
+            drive_docs = load_google_drive_documents()
 
         splits = split_documents(drive_docs)
         db = create_vector_store(splits)
         rag_chain = create_rag_chain(db)
         render_chat_interface(rag_chain)
-
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+        st.info("Please make sure all necessary secrets are properly configured in the Streamlit Cloud dashboard.")
 
 # Run the main function when the script is executed
 if __name__ == "__main__":
